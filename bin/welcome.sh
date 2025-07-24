8dcd916b663c3fe2721b789ea05e51a5bfa2e0d6
ELYSIA <contact@elysiaos.live>
2025-07-24 22:45:01 +0300

    Some updates

---
+++ bin/welcome.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#!/bin/bash
+~/.config/Elysia/Theme.sh
+python -m venv ~/myenv
+source ~/myenv/bin/activate
+pip install pypresence
+pip install youtube-search-python
+
+
+rm ~/bin/welcome.sh
+
+~/.config/hypr/welcoming/welcome
